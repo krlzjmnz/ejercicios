de964f7c60620aa1cd2ab19809a8cf208e43b987
Carlos Jimènez <carlos_jmnz@hotmail.com>
2019-05-07 21:06:41 -0500

    contador de letras

---
+++ 6 dada un string s imprimir cuantas letras tiene/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _6_dada_un_string_s_imprimir_cuantas_letras_tiene
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char seguir='y';
+            char[] cad;
+            do{
+                Console.WriteLine("Ingrese caracteres: ");
+                cad=Console.ReadLine().ToCharArray();
+
+                Console.WriteLine("En tus caracteres hay: "+numeroDeLetras(cad)+" Letras" );
+                
+                Console.WriteLine("Ingrese 'y' para volver a ingresar otros caracteres 'n' para salir");
+                if(!char.TryParse(Console.ReadLine(),out seguir) || seguir!='y' && seguir!='n'){
+                    Console.WriteLine("entrada no valida");
+                }
+            }while(seguir=='y');
+        }
+
+        public static int numeroDeLetras(char[] cadena){
+            int i=0;
+            foreach(char a in cadena){
+               if( (((int)a)>=65 && ((int)a)<=90) || (((int)a)>=97 && ((int)a)<=122)){
+                   i++;
+               } 
+            }
+            return i;
+        }
+    }
+}
---
+++ 7 dada un string s invertirla hola - aloh/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _7_dada_un_string_s_invertirla_hola___aloh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char seguir='y';
+            char[] cadena1, cadena2;
+            do{
+                Console.WriteLine("Ingresa una cadena para mostrarla invertida");
+                cadena1=Console.WriteLine();
+
+
+                Console.WriteLine("Ingrese 'y' para volver a ingresar otro numero 'n' para salir");
+                if(!char.TryParse(Console.ReadLine(),out seguir) || seguir!='y' && seguir!='n'){
+                    Console.WriteLine("entrada no valida");
+                }
+            }while(seguir=='y');
+        }
+    }
+}
