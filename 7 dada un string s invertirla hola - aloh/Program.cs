5aaa1de021b160f4b407e6247bcd6e82d16ae2cb
Carlos Jimènez <carlos_jmnz@hotmail.com>
2019-05-07 23:17:03 -0500

    palindromo

--- 7 dada un string s invertirla hola - aloh/Program.cs
+++ 7 dada un string s invertirla hola - aloh/Program.cs
@@ -10,14 +10,25 @@ namespace _7_dada_un_string_s_invertirla_hola___aloh
             char[] cadena1, cadena2;
             do{
                 Console.WriteLine("Ingresa una cadena para mostrarla invertida");
-                cadena1=Console.WriteLine();
-
-
-                Console.WriteLine("Ingrese 'y' para volver a ingresar otro numero 'n' para salir");
+                cadena1=Console.ReadLine().ToCharArray();
+                Invertir(cadena1,out cadena2);
+                Console.WriteLine("tu cadena invertida es:  ");
+                foreach(char a in cadena2){
+                    Console.Write(a);
+                }
+                Console.WriteLine("");
+                Console.WriteLine("Ingrese 'y' para volver a ingresar otro cadena'n' para salir");
                 if(!char.TryParse(Console.ReadLine(),out seguir) || seguir!='y' && seguir!='n'){
                     Console.WriteLine("entrada no valida");
                 }
             }while(seguir=='y');
+        }
+
+        public static void Invertir(char[] cad1, out char[] cad2){
+            cad2=new char[cad1.Length];
+            for(int i=0, j=cad1.Length-1; i<cad1.Length; i++, j-- ){
+                cad2[i]=cad1[j];
+            }
         }
     }
 }
---
+++ 8 determinar si una cadena es palindromo o no/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Text;
+
+namespace _8_determinar_si_una_cadena_es_palindromo_o_no
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char seguir='y';
+            char[] cadena;
+            do{
+                Console.WriteLine("Ingresa una oracion para saber si es palindromo");
+                cadena=quitarEspacios(Console.ReadLine().ToCharArray()).ToLower().ToCharArray();
+                
+                if(esPalindromo(cadena)){
+                    Console.WriteLine("La cadena es un palindromo");
+                }
+                else{
+                    Console.WriteLine("No es un palindromo");
+                }
+                Console.WriteLine("Ingrese 'y' para volver a ingresar otro numero 'n' para salir");
+                if(!char.TryParse(Console.ReadLine(),out seguir) || seguir!='y' && seguir!='n'){
+                    Console.WriteLine("entrada no valida");
+                }
+            }while(seguir=='y');
+        }
+        public static bool esPalindromo(char[] cad){
+            int aux =cad.Length/2, i=0, length=cad.Length-1;
+            while(i<=aux){
+                if(cad[i]!=cad[length]){
+                    return false;
+                }
+                i++;
+                length--;
+            }
+            return true;
+        }
+
+        public static string quitarEspacios(char[] cad){
+            StringBuilder cadena= new StringBuilder();
+            for(int i=0; i<cad.Length; i++){
+                if(cad[i]!=' '){
+                    cadena.Append(cad[i]);
+                }
+            }
+            return cadena.ToString();
+        }
+    }
+}
